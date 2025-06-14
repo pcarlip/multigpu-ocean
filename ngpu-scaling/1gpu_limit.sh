67f357753e012a814241661d24f00a873eb44ea4
pcarlip <carlipp@oregonstate.edu>
2025-06-13 22:35:54 -0700

    takes ~56% as long to run on a single GPU with no recording intermediate values or callbacks

--- ngpu-scaling/1gpu_limit.sh
+++ ngpu-scaling/1gpu_limit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=1gpu_limit
+#SBATCH --job-name=1gpu_slim
 #SBATCH --partition=ceoas-gpu
 #SBATCH --nodelist=aerosmith
 #SBATCH --cpus-per-task=6
@@ -9,7 +9,7 @@
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
 #SBATCH --time=3-00:00:00
-#SBATCH --output=1024^3.out
+#SBATCH --output=1gpu_slim.out
 
 export MPITRAMPOLINE_LIB="/local/ceoas/x86_64/opt/MPIwrapper/openmpi@4.1.6%gcc@13.2-cuda-sandybridge/lib64/libmpiwrapper.so"
 export MPITRAMPOLINE_MPIEXEC="/local/ceoas/x86_64/opt/MPItrampoline/openmpi@4.1.6%gcc@13.2-cuda-sandybridge/bin/mpiwrapperexec"
@@ -21,4 +21,4 @@ export JULIA_NUM_THREADS=1
 # export CUDA_VISIBLE_DEVICES=0,1,2,3
 # use the above to limit available GPUs
 
-julia --project=.. 1gpu-limit.jl
+julia --project=.. 1gpu-limit-slim.jl
