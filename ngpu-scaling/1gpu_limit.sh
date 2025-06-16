b37e7f61008c1e5d9990caabd4ad79d1b01f040a
pcarlip <carlipp@oregonstate.edu>
2025-06-16 01:01:47 -0700

    redo metrics for 1 gpu

--- ngpu-scaling/1gpu_limit.sh
+++ ngpu-scaling/1gpu_limit.sh
@@ -9,7 +9,7 @@
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
 #SBATCH --time=3-00:00:00
-#SBATCH --output=1gpu_slim.out
+#SBATCH --output=1gpu_final.out
 
 export MPITRAMPOLINE_LIB="/local/ceoas/x86_64/opt/MPIwrapper/openmpi@4.1.6%gcc@13.2-cuda-sandybridge/lib64/libmpiwrapper.so"
 export MPITRAMPOLINE_MPIEXEC="/local/ceoas/x86_64/opt/MPItrampoline/openmpi@4.1.6%gcc@13.2-cuda-sandybridge/bin/mpiwrapperexec"
@@ -21,4 +21,4 @@ export JULIA_NUM_THREADS=1
 # export CUDA_VISIBLE_DEVICES=0,1,2,3
 # use the above to limit available GPUs
 
-julia --project=.. 1gpu-limit-slim.jl
+julia --project=.. 1gpu-limit.jl
