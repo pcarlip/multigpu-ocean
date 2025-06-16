5bdfc53ac0be70839aa7f913a86ad496bdb78bf2
pcarlip <carlipp@oregonstate.edu>
2025-06-16 00:39:36 -0700

    can't do a direct comparison for 3 GPUs, since it wants to evenly divide the domain

---
+++ ngpu-scaling/3gpu_mpi.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --job-name=3gpu_mpi
+#SBATCH --partition=ceoas-gpu
+#SBATCH --nodelist=aerosmith
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=100000
+#SBATCH --nodes=1
+#SBATCH --ntasks=3
+#SBATCH --gres=gpu:3
+#SBATCH --time=3-00:00:00
+#SBATCH --output=3gpu_mpi.out
+
+export MPITRAMPOLINE_LIB="/local/ceoas/x86_64/opt/MPIwrapper/openmpi@4.1.6%gcc@13.2-cuda-sandybridge/lib64/libmpiwrapper.so"
+export MPITRAMPOLINE_MPIEXEC="/local/ceoas/x86_64/opt/MPItrampoline/openmpi@4.1.6%gcc@13.2-cuda-sandybridge/bin/mpiwrapperexec"
+
+export UCX_WARN_UNUSED_ENV_VARS=n
+
+export JULIA_NUM_THREADS=1
+
+# export CUDA_VISIBLE_DEVICES=0,1,2,3
+# use the above to limit available GPUs
+
+/local/ceoas/x86_64/opt/MPIwrapper/openmpi@4.1.6%gcc@13.2-cuda-sandybridge/bin/mpiwrapperexec -n 3 julia --project=.. 2gpu-mpi.jl
