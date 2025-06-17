90ef621cbfcf73844829193448aafc74d9f3bc02
pcarlip <peter.carlip@proton.me>
2025-06-16 17:16:16 -0700

    scaling is fairly similar for 3/4 size sim; run time is quite close to 3/4 the larger sim and memory approaches that for 4 GPUs

--- odd/odd-comp.jl
+++ odd/odd-comp.jl
@@ -24,8 +24,85 @@ mem_tot = mem_per .* n
 times = [4.859, 12.4835, 9.607]
 
 # %%
-n_o = [1, 2, 3, 4]
-times_o = [3.818, 9.48]
-mem_per_o = [53600, 40000]
-mem_tot_o = n_o .* mem_per_o
+times_o = [3.818, 9.48, 7.23]
+mem_per_o = [53600, 40000, 17575]
+mem_tot_o = n .* mem_per_o
 
+
+
+# %%
+(times_o ./ 0.75) ./ times
+
+
+# %%
+(mem_tot_o ./ 0.75) ./ mem_tot
+
+
+# %%
+plot(
+    n,
+    mem_tot,
+    linewidth = 2,
+    label = "Total memory (power of 2)",
+    shape = :circle,
+    markersize = 3,
+)
+plot!(
+    n,
+    mem_per,
+    linewidth = 2,
+    label = "Memory per GPU (power of 2)",
+    shape = :circle,
+    markersize = 3,
+)
+plot!(
+    n,
+    mem_tot_o,
+    linewidth = 2,
+    label = "Total memory (non-power of 2)",
+    shape = :circle,
+    markersize = 3,
+)
+plot!(
+    n,
+    mem_per_o,
+    linewidth = 2,
+    label = "Memory per GPU (non-power of 2)",
+    shape = :circle,
+    markersize = 3,
+)
+
+plot!(legend = :bottomleft)
+xlabel!("Number of GPUs")
+ylabel!("Memory (MiB)")
+ylims!(0, 10^5)
+title!("Memory use scaling")
+
+
+
+# %%
+plot(
+    n,
+    times,
+    linewidth = 2,
+    label = "Run time (power of 2)",
+    shape = :circle,
+    markersize = 3,
+)
+plot!(
+    n,
+    times_o,
+    linewidth = 2,
+    label = "Run time (non-power of 2)",
+    shape = :circle,
+    markersize = 3,
+)
+
+xlabel!("Number of GPUs")
+ylabel!("Simulation time (min)")
+ylims!(0, 13)
+title!("Time scaling")
+
+
+
+# %%
