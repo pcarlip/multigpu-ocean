6558c2c6211ff815f1ca2eefe5bd748b4cbc2c28
pcarlip <peter.carlip@proton.me>
2026-09-23 02:36:04 -0700

    clean up cqls files, prep for more coe sims

--- ngpu-scaling/2gpu_limit.sh
+++ ngpu-scaling/2gpu_limit.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#SBATCH --job-name=1gpu-oc
+#SBATCH --job-name=2gpu-oc
 #SBATCH --partition=dgxh
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=50G
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --gres=gpu:1
+#SBATCH --ntasks=2
+#SBATCH --gres=gpu:2
 #SBATCH --time=3:00:00
-#SBATCH --output=1gpu.out
+#SBATCH --output=2gpu.out
 
 module load openmpi/4.1_gcc-12 cuda/13.0 julia/1.12
 export JULIA_CUDA_MEMORY_POOL=none
-mpiexec -n 1 julia --project=.. ../generic_sim.jl 1gpu_limit.toml
+mpiexec -n 2 julia --project=.. ../generic_sim.jl 2gpu_limit.toml
